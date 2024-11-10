b131743477ff4ea94e71be2df155847f9a8df60c
nrmnqdds <quddusnuriman@gmail.com>
2024-11-11 01:39:08 +0800

    wip

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -6,7 +6,7 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight-night]])
+      vim.cmd("colorscheme tokyonight-night")
     end,
   },
 }
